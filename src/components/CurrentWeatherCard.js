37dae3ac07eca517ac857a41357f7e5c6ba17b12
shenjiajun <shenjj@2345.com>
2016-12-02 18:16:53 +0800

    wwww

--- src/components/CurrentWeatherCard.js
+++ src/components/CurrentWeatherCard.js
@@ -6,7 +6,11 @@
 import React from 'react';
 // import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import {Card} from 'antd';
+import {Row, Col} from 'antd';
 import 'antd/dist/antd.css';
+
+// import hdpi from '../../res/drawable-hdpi';
+const high=require("../../res/drawable-hdpi/currnet_high_b.png");
 
 const defStyle = {
     marginRight: '100px',
@@ -32,51 +36,53 @@ class CurrentWeatherCard extends React.Component {
                 <div className="card_line"></div>
 
                 <div className=" grey lighten-3">
-                    <div className="row no_margin">
-                        <div id="temper_high_div" className="container_margin col-md-3 col-xs-6 text-center">
+                    <Row >
+                        <Col span={6} id="temper_high_div">
                             <div>高</div>
-                            <img src="./res/drawable-hdpi/currnet_high_b.png"/>
+                            <img src="../../res/drawable-hdpi/currnet_high_b.png"/>
                             <div id="temp_high_value"></div>
-                        </div>
-                        <div id="temper_low_div" className="container_margin col-md-3 col-xs-6 text-center">
+                        </Col>
+                        <Col span={6} id="temper_low_div">
                             <div>低</div>
-                            <img src="./res/drawable-hdpi/currnet_low_b.png"/>
+                            <img src={require("../../res/drawable-hdpi/currnet_high_b.png")}/>
                             <div id="temper_low_value"></div>
-                        </div>
-                        <div id="precipitation_div" className="container_margin col-md-3 col-xs-6 text-center">
+                        </Col>
+                        <Col span={6} id="precipitation_div">
                             <div>降水</div>
                             <img src="./res/drawable-hdpi/currnet_preciptation_b.png"/>
                             <div id="precipitation_value"></div>
-                        </div>
-                        <div id="humidity_div" className="container_margin col-md-3 col-xs-6 text-center">
+                        </Col>
+                        <Col span={6} id="humidity_div">
                             <div>湿度</div>
                             <img src="res/drawable-hdpi/currnet_humidity_b.png"/>
                             <div id="humidity_value"></div>
-                        </div>
-                    </div>
+                        </Col>
+                    </Row>
 
-                    <div className="row no_margin">
-                        <div id="visibility_div" className="container_margin col-md-3 col-xs-6 text-center">
+                    <Row className="row no_margin">
+                        <Col span={6} id="visibility_div">
                             <div>可见度</div>
                             <img src="res/drawable-hdpi/currnet_visibility_b.png"/>
                             <div id="visibility_value"></div>
-                        </div>
-                        <div id="wind_div" className="container_margin col-md-3 col-xs-6 text-center">
+                        </Col>
+                        <Col span={6} id="wind_div">
                             <div>风速</div>
                             <img src="res/drawable-hdpi/currnet_wind_b.png"/>
                             <div id="wind_value"></div>
-                        </div>
-                        <div id="pressure_div" className="container_margin col-md-3 col-xs-6 text-center">
+                        </Col>
+                        <Col span={6} id="pressure_div">
                             <div>气压</div>
                             <img src="res/drawable-hdpi/currnet_pressure_b.png"/>
                             <div id="pressure_value"></div>
-                        </div>
-                        <div id="uv_index_div" className="container_margin col-md-3 col-xs-6 text-center">
+                        </Col>
+                        <Col span={6} id="uv_index_div">
                             <div>紫外线指数</div>
                             <img src="res/drawable-hdpi/currnet_uv_b.png"/>
                             <div id="uv_index_value"></div>
-                        </div>
-                    </div>
+                        </Col>
+                    </Row>
+
+
                 </div>
             </Card>
         )
